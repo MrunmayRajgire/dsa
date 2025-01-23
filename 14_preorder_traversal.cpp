5a5321f28012c3c0f4723423bc1e5d2ce153df4f
MrunmayRajgire <113378987+MrunmayRajgire@users.noreply.github.com>
2025-01-23 22:31:21 +0530

    preorder

---
+++ 14_preorder_traversal.cpp
@@ -0,0 +1,15 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+void preorder(node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+    cout<<node->data;
+    preorder(node->left);
+    preorder(node->right);
+}
+*/
